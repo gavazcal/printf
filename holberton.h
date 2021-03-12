e362219093d00d5bad3153b973dd996aa5cb3f3c
gavazcal <2392@holbertonschool.com>
2021-03-12 20:41:57 +0000

    Adding a primitive header

---
+++ holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/*LIBRARIES*/
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+/*PROTOTYPES*/
+int _putchar(char c);
+
+/*PLACEHOLDER FOR FUTURE FUNCTIONS*/
+
+/*PROBABLY GONNA NEED A STRUCT IN HERE SOMEWHERE*/
+
+#endif /* HOLBERTON_H */
