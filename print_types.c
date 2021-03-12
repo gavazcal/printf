d0c819ca343b58dc1a458ec280e1495bb5adbc75
gavazcal <2392@holbertonschool.com>
2021-03-12 23:37:09 +0000

    Adding temp work

---
+++ print_types.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_single_char - prints one char
+ * @args: arguments
+ * Return: 1 on success
+ */
+int print_single_char(va_list args)
+{
+	return (_putchar(va_arg(args, int)));
+}
+int print_string(va_list args)
+{
+;
+}
+
+int print_int(va_list args)
+{
+;
+}
+
+
