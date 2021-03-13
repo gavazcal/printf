5ea05b2e11d5cdd6f1f6db81d54bc73324882559
gavazcal <2392@holbertonschool.com>
2021-03-13 16:23:09 +0000

    adding print char, string and modulus functions

--- print_types.c
+++ print_types.c
@@ -8,14 +8,43 @@ int print_single_char(va_list args)
 {
 	return (_putchar(va_arg(args, int)));
 }
+/**
+ * print_string - prints a string
+ * @args: arguments
+ * Return: chars printed
+ */
 int print_string(va_list args)
 {
-;
+	char *string = va_arg(args, char *);
+	int idx;
+
+	if (string == NULL)
+	{
+		string = "(null)";
+	}
+	for (idx = 0; string[idx]; idx++)
+	{
+		_putchar(string[idx]);
+	}
+	return (idx);
 }
 
+/**
+ * print_int prints out integers
+ * @args: arguments
+ * Return: integer
+ */
 int print_int(va_list args)
 {
 ;
 }
 
-
+/**
+ * print_modulus - prints a % when asked
+ * @args: arguments (unused)
+ * Return: 1
+ */
+int print_modulus(va_list __attribute__((unused)) list)
+{
+	return(_putchar('%');
+}
