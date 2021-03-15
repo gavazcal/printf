cbbdd7356187307198ba3cd1dd54524594fe555d
yatoro667 <ytorres667@gmail.com>
2021-03-15 23:07:33 +0000

    Adding comments to find_type.c and _printf prototype

---
+++ _printf.c
@@ -0,0 +1,8 @@
+#include 
+#include
+#include
+
+_printf(const char *format, arg1, arg2, ...);
+_printf(char *format, arg1, arg2, ...);
+
+/* Not sure if const char or char */
--- find_type.c
+++ find_type.c
@@ -1,3 +1,32 @@
 #include "holberton.h"
 
+/* Specifiers */
 
+
+%% - percent sign (/%)
+%c - char
+%d, %i - signed int
+%e, %E - long int /* scientific notation */
+%f - float
+%g, %G - short /* uses shorter of %e or %f; %E or %f */
+%o - signed int /* octal */
+%s - char * /* string */
+%u - unsigned int
+%x, %X - unsigned int /* hexadecimal */
+%p - * /* pointer address  & is address-of mod */
+%n - /* nothing void? */
+
+/* Width - %5d space fill %05d 0 fill)
+ * Precision - floats, %.6 (default)
+ * Signage - %
+ * Justifaction - right-justified default; - left align, + right align
+ * Fill - space
+ * */
+
+struct type_finder {
+	{"c", print_single_char},
+	{"s", print_string},
+	{"d", print_int},
+	{"i", print_int},
+	{"%", print_modulus},
+};
