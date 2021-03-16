491e270f14d1cb01f6d70a4f9aa4e9926af7aea5
yatoro667 <ytorres667@gmail.com>
2021-03-16 03:03:03 +0000

    Added int function

--- print_types.c
+++ print_types.c
@@ -36,7 +36,65 @@ int print_string(va_list args)
  */
 int print_int(va_list args)
 {
-;
+	int num = va_arg(args, int);
+	int numsize = 0;
+	int *intsize; /* For alt method */
+
+	print_int2(num);
+
+	if (num < 0)
+		numsize = 1;
+	while (num =! 0)
+	{
+		num /= 10;
+		numsize++;
+	}
+	return (numsize);
+
+	/* Alternative reverse/forward printing */
+
+	/* Can use a count function to find digits then create array based on digit size */
+
+	intlimit = malloc(sizeof(int) * 20); /* 20 is int limit */
+	/* or */
+	intlimit = malloc(sizeof(int) * numsize); /* Memory efficienct */
+
+	int idx;
+	int intcopy;
+
+	for (idx = 0; num =! 0; idx++)
+	{
+		intcopy = num % 10;
+		intsize[idx] = intcopy;
+		num /= 10;
+	}
+	while (idx > -1)
+	{
+		_putchar(intsize[idx] = '0';
+	idx--;
+	}
+	free(intsize);
+	return (num);
+}
+
+/* 2nd function */
+
+int print_int2(int n)
+{
+	unsigned int num;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+	if (num / 10 != 0)
+	{
+		print_int2(num / 10);
+	}
+	_putchar(num % 10 + '0');
 }
 
 /**
