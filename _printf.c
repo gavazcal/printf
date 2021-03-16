c45cf09b5d96652e99bf178654744ef7e406f8c9
gavazcal <2392@holbertonschool.com>
2021-03-16 02:07:12 +0000

    adding main printf

--- _printf.c
+++ _printf.c
@@ -1,8 +1,52 @@
-#include 
-#include
-#include
+#include "holberton.h"
 
-_printf(const char *format, arg1, arg2, ...);
-_printf(char *format, arg1, arg2, ...);
+/**
+ * _printf - main printf function
+ * @format: the string passed
+ * Return: length of string
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int (*f)(va_list);
+	int idx = 0; strlen = 0;
+	char pointer;
 
-/* Not sure if const char or char */
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	for (idx = 0; format[idx] != '\0'; idx++)
+	{
+		if (format[idx] == '%')
+		{
+			if (format[idx + 1] == '\0')
+				return (-1);
+			else if (format[idx + 1] == '%')
+			{
+				_putchar('%');
+				idx++;
+				strlen += 1;
+			}
+			else
+			{
+				pointer = format[idx + 1];
+				f = find_type(&pointer);
+				if (f == NULL)
+					return (strlen);
+				else
+				{
+					strlen += f(args);
+					idx++;
+				}
+			}
+		}
+		else
+		{
+			write(1, &format[idx], 1);
+			strlen += 1;
+		}
+	}
+	va_end(args);
+	return (strlen);
+}
